fa288dd2251721952096b6c3b2d0c7e9155eef0a
msolanogithub <msolano@imaginacolombia.com>
2025-05-14 17:32:25 -0500

    first base commit

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $bootTime = round((microtime(true) - LARAVEL_START) * 1000, 2);
+  $bootTime = round((microtime(true) - LARAVEL_START) * 1000, 2);
 
-    return response()->json([
-        'app' => config('app.name'),
-        'status' => 'OK',
-        'boot_time_ms' => $bootTime,
-        'php_version' => PHP_VERSION,
-        'laravel_version' => app()->version(),
-        'timestamp' => now()->toIso8601String(),
-    ]);
+  return response()->json([
+    'app' => config('app.name'),
+    'status' => 'OK',
+    'boot_time_ms' => $bootTime,
+    'php_version' => PHP_VERSION,
+    'laravel_version' => app()->version(),
+    'timestamp' => now()->toIso8601String(),
+  ]);
 });
