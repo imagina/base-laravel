ab54650a2463f4d1a63352bf6d04c1187796f2ee
msolanogithub <msolano@imaginacolombia.com>
2025-05-22 11:45:26 -0500

    updated icrud by icore

---
+++ packages/imagina/icore/src/providers/IcoreServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Imagina\Icore\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Imagina\Icore\Routes\RouterGenerator;
+
+class IcoreServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        // Register apiCrud as a router macro
+        Route::macro('apiCrud', function ($params) {
+            app(RouterGenerator::class)->apiCrud($params);
+        });
+    }
+
+    public function register(): void
+    {
+        //
+    }
+}
