53e0c1aabbf62e0b2a05eb45de318997c94a1667
msolanogithub <msolano@imaginacolombia.com>
2025-05-28 09:52:00 -0500

    fix icore

--- packages/imagina/icore/src/Http/Controllers/CoreApiController.php
+++ packages/imagina/icore/src/Http/Controllers/CoreApiController.php
@@ -25,9 +25,9 @@ abstract class CoreApiController
             'order' => $request->input('order'),
             'page' => $request->input('page', 1),
             'take' => $request->input('take', 12),
-            'filter' => json_decode($request->input('filter', '[]')),
-            'include' => explode(',', $request->input('include', '')),
-            'fields' => explode(',', $request->input('fields', ''))
+            'filter' => json_decode($request->input('filter', '[]'), true) ?? [],
+            'include' => array_filter(explode(',', $request->input('include', '')), fn($val) => $val !== ''),
+            'fields' => array_filter(explode(',', $request->input('fields', '')), fn($val) => $val !== ''),
         ];
     }
 
